5bee2d9d8c968c83513916c750cdb76ee5d47972
Hunter Wallen <hunterwallen67@gmail.com>
2022-11-15 19:15:33 -0700

    w2d1 notes

---
+++ 1_front_end_development/week_02/1_tue/instructor_examples/first_code.js
@@ -0,0 +1,204 @@
+
+// //////////////////////////////////////////////////
+// This line is printing hello world to the terminal
+// some people do multiline
+// like this as well
+// //////////////////////////////////////////////////
+/*
+This
+is
+a
+multi
+line
+comments
+*/
+
+// console.log('Hello world')
+// console.log("Javascript doesn't care")
+// console.log(`This is wrapped in backticks`)
+
+// console.log(typeof NaN)
+
+// const firstName = 'Hunter'
+// firstName = 'Brad'
+// let myAge = 31
+// myAge = 40
+// var dogsName = 'Nero'
+// dogsName =  'Kurt'
+
+// console.log('Hello ' + firstName + '. You are ' + myAge + ' years old and I have a husky named ' + dogsName)
+
+// console.log('Hello, Hunter')
+// console.log('Hello, Raaahima')
+
+
+// let a = '';
+// let b = 'bongos';
+// let c = 'get your';
+
+// a = b; // a becomes 'bongos
+// b = c; // b becomes get your
+// c = a; // c becomes 'bongos'
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// console.log('hello ' + 'world')
+
+// console.log('Come ' + c + ' ' + b + '!')
+
+// const nameSentence = 'I am ' + 31 + ' years old'
+
+// console.log(typeof nameSentence)
+
+
+// console.log('Please ' +  'squeeze ' + 'the ' + 'cheese')
+
+// const word = 'believe'
+// console.log('Please ' + word + ' the ' + 'cheese')
+
+// const five = 5
+// const ten = 10
+// const sum = five + ten
+
+// console.log('The sum of ' + five + ' and ' +  ten + ' equals ' + (five + ten))
+
+// const thisIsTrue = true
+// console.log(typeof thisIsTrue)
+
+
+// const password = 'p1234'
+// console.log(password === 'p1234')
+
+// const sale = '100'
+// console.log(sale == 100)
+// console.log(sale === 100)
+
+
+// const userAge = 13
+// console.log(13 <= userAge)
+
+// const shoppingCartTotal = 111
+// console.log(50 < shoppingCartTotal)
+
+
+// const oldPassword = 'p1234'
+// const newPassword = 'password1234'
+// console.log(oldPassword !== newPassword)
+
+// console.log(1 == 1)
+// console.log('beans' === 'Beans')
+// console.log((5 + 5 * 3) === ((5+5) * 3))
+
+// console.log(9 !== false)
+// console.log('A' > 'a')
+// console.log('Burger King' > 'McDonalds')
+// console.log(NaN == NaN)
+ 
+// const fanOfCher = true
+// const livesInNY = 'asdf'
+// const sendText = livesInNY && !!fanOfCher
+
+// console.log( sendText )
+
+// const jacket = 'green'
+// console.log(jacket === 'blue' || jacket === 'black')
+
+
+
+// let myName = 'Hunter'
+
+// myName = myName + ' Wallen'
+
+// console.log(myName)
+
+// console.log(!false && true)
+
+// console.log(false || true)
+
+
+// const username = ''
+// let user = username || 'Bob Bobby Bob'
+// console.log(user)
+
+// let num = 1
+
+// while(num <= 1000){
+//   console.log('The number is: ' + num)
+  // num++
+  // num += 1
+  // num = num + 1
+// }
+
+// let runProgram = true
+// while(runProgram){
+//   console.log('progam is running')
+//   runProgram = false
+// }
+ 
+
+// num = num - 1
+// num--
+// num -= 1
+
+// for(let i = 1; i <= 1000; i += 1000) {
+//   console.log('The number is: ' + i)
+// }
+
+
+// for(let i = 999; i <= 9999; i+=1){
+//   console.log(i)
+// }
+
+
+// for(let i = 100; i >= 0; i--){
+//   console.log(i)
+// }
+
+
+
+// Send an email to every student in my class
+
+// GET a list of students in the class
+// FOR every student in the class
+  // FIND their email
+  // CREATE new email to thier email
+  // ADD email contents to body
+  // PRESS SEND
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
