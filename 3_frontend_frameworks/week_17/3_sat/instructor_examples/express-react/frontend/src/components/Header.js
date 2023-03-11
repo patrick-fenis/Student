0cd0f826cf6453dfe4622fc3e08c81e6bf40bd9c
Hunter Wallen <hunterwallen67@gmail.com>
2023-03-11 11:55:52 -0700

    example code

---
+++ 3_frontend_frameworks/week_17/3_sat/instructor_examples/express-react/frontend/src/components/Header.js
@@ -0,0 +1,18 @@
+// const Header = (props) => {
+//   return <h1>Header</h1>
+// }
+
+// export default Header
+
+
+import {Link} from 'react-router-dom'
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (props) => {
+  return (
+    <nav className='nav'>
+      <Link to='/'>
+        <div>People App</div>
+      </Link>
+    </nav>
+  )
+}
